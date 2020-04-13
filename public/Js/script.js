85b90041a12fe7c39dd61561fbae5a195307dcf5
oceane97440 <sautronoceane98@gmail.com>
2020-04-13 17:21:28 +0400

    commit

--- public/Js/script.js
+++ public/Js/script.js
@@ -5,45 +5,45 @@ $(document).ready(function () {
 
     selectmarque.on("change", function() {
         $("#cardinfo").hide();
-        affichemodel($(this).val());
+        model($(this).val());
     })
 
     selectmodel.on("change", function() {
-        afficheunmodel($(this).find(":selected").data("brand"), $(this).val());
+        info($(this).find(":selected").data("marque"), $(this).val());
     })
 
-    function affichemodel(idmarque) {
+    function model(idmarque) {
         $.get(`/${idmarque}/model/`, function(info) {
-            let carModels = info.makes.Models; 
+            let modelvoiture = info.makes.Models; 
             
-            let models = `<option value="" selected disabled>Veuillez chosir un modèle</option>`;
+            let listemodel = `<option value="" selected disabled>Veuillez chosir un modèle</option>`;
             
     
             
-            carModels.forEach((model) => {
-                models += `<option value="${model.model_name}" data-brand="${model.model_make_id}">${model.model_name}</option>`
+            modelvoiture.forEach((model) => {
+                listemodel += `<option value="${model.model_name}" data-marque="${model.model_make_id}">${model.model_name}</option>`
             });
 
-            $("#model").html(models);
+            $("#model").html(listemodel);
 
         })
     }
 
 
-    function afficheunmodel(idBrand, idModel) {
-        $.get(`/${idBrand}/${idModel}/model`, function(result) {//GET:/idmarque/idmodel/model
+    function info(idmarque, idmodel) {
+        $.get(`/${idmarque}/${idmodel}/model`, function(card) {//GET:/idmarque/idmodel/model
 
       
             //recup les id des li de la cart et recup les donné dans le tableau trims (info.Trism[])
-            $("#voiture-nom").html('<h6>Noms du model:</h6>'+result.info.Trims[0].model_name);
-            $("#voiture-finition").html('<h6>Marque:</h6>'+result.info.Trims[0].model_make_id);
-            $("#voiture-prix").html('<h6>Année sortie:</h6>'+result.info.Trims[0].model_year);
-            $("#voiture-energie").html('<h6>Type essence:</h6>'+result.info.Trims[0].model_engine_fuel);
-            $("#voiture-boiteDeVitesse").html('<h6>Transmission:</h6>'+result.info.Trims[0].model_transmission_type);
-            $("#voiture-puissance").html('<h6>Poxer energie:</h6>'+result.info.Trims[0].model_engine_power_rpm);
-            $("#voiture-kg").html('<h6>Nbr de kg:</h6>'+result.info.Trims[0].model_weight_kg);
-            $("#voiture-speed").html('<h6>Meilleur speed</h6>'+result.info.Trims[0].model_top_speed_kph);
-            $("#voiture-kph").html('<h6>Kph:</h6>'+result.info.Trims[0].model_0_to_100_kph);
+            $("#voiture-nom").html('<h6>Noms du model:</h6>'+card.info.Trims[0].model_name);
+            $("#voiture-finition").html('<h6>Marque:</h6>'+card.info.Trims[0].model_make_id);
+            $("#voiture-prix").html('<h6>Année sortie:</h6>'+card.info.Trims[0].model_year);
+            $("#voiture-energie").html('<h6>Type essence:</h6>'+card.info.Trims[0].model_engine_fuel);
+            $("#voiture-boiteDeVitesse").html('<h6>Transmission:</h6>'+card.info.Trims[0].model_transmission_type);
+            $("#voiture-puissance").html('<h6>Poxer energie:</h6>'+card.info.Trims[0].model_engine_power_rpm);
+            $("#voiture-kg").html('<h6>Nbr de kg:</h6>'+card.info.Trims[0].model_weight_kg);
+            $("#voiture-speed").html('<h6>Meilleur speed</h6>'+card.info.Trims[0].model_top_speed_kph);
+            $("#voiture-kph").html('<h6>Kph:</h6>'+card.info.Trims[0].model_0_to_100_kph);
 
             
             $("#cardinfo").show();
